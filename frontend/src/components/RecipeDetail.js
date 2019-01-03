621341acb5ef2a1f0a2f49d68be1417c0e94d413
Caitlin McKenna <caitlin.elizamckenna@gmail.com>
2019-01-03 09:54:37 -0700

    Clean up in-component styles by bringing them into one const object, refactor turnaries
    (cherry picked from commit bc501b41092a93721c1849f01729ccb701674690)

--- frontend/src/components/RecipeDetail.js
+++ frontend/src/components/RecipeDetail.js
@@ -1,5 +1,50 @@
 import React, { Component } from 'react';
 
+const Style = {
+    main : {
+        display: 'grid',
+        gridTemplateColumns: '2fr 3fr',
+        gridGap: '3rem',
+        marginTop: '3em'
+    },
+    span : {
+        fontSize: '.8em',
+        color: 'white',
+        fontWeight: 'bold',
+        display: 'inline-block',
+        padding: '3px 10px',
+    },
+    spanGF : {
+        background: '#E26D5A',
+    },
+    spanKT : {
+        background: '#533745'
+    },
+    h1 : {
+        marginBottom: '.2em'
+    },
+    ingredients : {
+        textAlign: 'left',
+        border: '1px solid #ccc',
+        padding: '10px 30px'
+    },
+    ingredient : {
+        marginBottom: '.8em'
+    },
+    instructions : {
+        textAlign: 'left',
+    },
+    img : {
+        width: '75px',
+        height: '75px',
+        borderRadius: '100%',
+        backgroundSize:'cover',
+        backgroundPosition: 'center',
+        display: 'inline-block',
+        marginRight: '15px',
+        verticalAlign: 'text-bottom'
+    }
+}
 
 export default class RecipeDetail extends Component {
 
@@ -24,77 +69,41 @@ export default class RecipeDetail extends Component {
 
 
   render() {
-    const { recipe } = this.state;
-    const spanStyle = {
-        fontSize: '.8em',
-        color: 'white',
-        fontWeight: 'bold',
-        display: 'inline-block',
-        padding: '3px 10px',
-    }
-    const spanStyleGF = {
-        background: '#E26D5A',
-    }
-    const spanStyleKT = {
-        background: '#533745'
-    }
-    const styleMain = {
-        display: 'grid',
-        gridTemplateColumns: '2fr 3fr',
-        gridGap: '3rem',
-        marginTop: '3em'
-    }
-    const h1Style = {
-        marginBottom: '.2em'
-    }
-    const imgStyle = {
-        width: '75px',
-        height: '75px',
-        borderRadius: '100%',
-        backgroundImage: `url(${recipe.image})`,
-        backgroundSize:'cover',
-        backgroundPosition: 'center',
-        display: 'inline-block',
-        marginRight: '15px',
-        verticalAlign: 'text-bottom'
-    }
-    const ingredientsStyle = {
-        textAlign: 'left',
-        border: '1px solid #ccc',
-        padding: '10px 30px'
-    }
-    const ingredientStyle = {
-        marginBottom: '.8em'
-    }
-    const styleInstructions = {
-        textAlign: 'left',
-    }
+  const { recipe } = this.state;
+  const imgBackground = {
+      backgroundImage: `url(${recipe.image})`,
+  }
 
-    return (
-      <div className="recipeDetail">
-        <h1 style={h1Style}>
-            <div style={imgStyle}>&nbsp;</div>
-            {recipe.title}
-        </h1>
-        {recipe.glutenFree ? <span style={{...spanStyle, ...spanStyleGF}}>Gluten free</span> : null }
-        {recipe.ketogenic ? <span style={{...spanStyle, ...spanStyleKT}}>Keto</span> : null }
-        <div className="main" style={styleMain}>
-            {recipe.extendedIngredients
-                ?
-                <div className="ingredients" style={ingredientsStyle}>
-                    <h3>Ingredients</h3>
-                    {recipe.extendedIngredients.map(item => (
-                        <div style={ingredientStyle}>
-                            {item.measures.us.amount} {item.measures.us.unitShort} {item.name}
-                        </div>
-                    ))}
+    if(!recipe){
+        return (
+            <h3>Loading...</h3>
+        )
+    }
+    else{
+        return (
+          <div className="recipeDetail">
+            <h1 style={Style.h1}>
+                <div style={{...Style.img, ...imgBackground}}>&nbsp;</div>
+                {recipe.title}
+            </h1>
+            {recipe.glutenFree && <span style={{...Style.span, ...Style.spanGF}}>Gluten free</span>}
+            {recipe.ketogenic && <span style={{...Style.span, ...Style.spanKT}}>Keto</span>}
+            <div className="main" style={Style.main}>
+                {recipe.extendedIngredients &&
+                    <div className="ingredients" style={Style.ingredients}>
+                        <h3>Ingredients</h3>
+                        {recipe.extendedIngredients.map(item => (
+                            <div style={Style.ingredient}>
+                                {item.measures.us.amount} {item.measures.us.unitShort} {item.name}
+                            </div>
+                        ))}
+                    </div>}
+                <div className="instructions" style={Style.instructions}>
+                    {recipe.instructions}
                 </div>
-                : null }
-            <div className="instructions" style={styleInstructions}>
-                {recipe.instructions}
             </div>
-        </div>
-      </div>
-    );
+          </div>
+        );
+    }
   }
 }
--- frontend/src/components/RecipeLink.js
+++ frontend/src/components/RecipeLink.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 
+const Style = {
+    recipe : {
+        width: '100%',
+        minHeight: '300px',
+        backgroundColor: '#ccc',
+        backgroundSize: 'cover',
+        display: 'block',
+        backgroundPosition: 'center',
+        position: 'relative',
+        borderRadius: '4px',
+    },
+    h2 : {
+        color: '#fff',
+        position: 'absolute',
+        top: '30%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        textAlign: 'center',
+        fontSize: '1.65rem',
+        textShadow: '2px 4px 3px rgba(0,0,0,0.2)',
+        textTransform: 'uppercase',
+        width: '80%',
+    },
+
+}
+
 export default class RecipeLink extends Component {
   render() {
 
-  const background = this.props.baseUrl + this.props.image;
-  const recipeStyle = {
-      width: '100%',
-      minHeight: '300px',
-      background: `linear-gradient(
-      rgba(0, 0, 0, 0.4),
-      rgba(0, 0, 0, 0.5)
-    ), url(${background})`,
-      backgroundColor: '#ccc',
-      backgroundSize: 'cover',
-      display: 'block',
-      backgroundPosition: 'center',
-      position: 'relative',
-      borderRadius: '4px',
-  };
-  const h2Style = {
-      color: '#fff',
-      position: 'absolute',
-      top: '30%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      textAlign: 'center',
-      fontSize: '1.65rem',
-      textShadow: '2px 4px 3px rgba(0,0,0,0.2)',
-      textTransform: 'uppercase',
-      width: '80%',
-  }
+      const background = this.props.baseUrl + this.props.image;
+      const backgroundStyle = {
+          background: `linear-gradient(
+          rgba(0, 0, 0, 0.4),
+          rgba(0, 0, 0, 0.5)
+        ), url(${background})`,
+      };
 
     return (
       <Link to={`/${this.props.id}`}>
-          <div style={recipeStyle}>
-            <h2 style={h2Style}>{this.props.title}</h2>
+          <div style={{...Style.recipe, ...backgroundStyle}}>
+            <h2 style={Style.h2}>{this.props.title}</h2>
           </div>
       </Link>
     );
